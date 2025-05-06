ee8265ae0d9e4e9ddf4707505ea26f562f7a070a
Elsarrag-Apps <esamelsarrag@hoarelea.com>
2025-05-06 19:42:12 +0100

    Update heatmap_app.py

--- heatmap_app.py
+++ heatmap_app.py
@@ -241,6 +241,14 @@ elif mode == "Building Overheating Risk":
                   """
               )
 
+           st.markdown("""
+          <small>
+          <strong>2°C:</strong> Low global warming scenario<br>
+          <strong>3°C:</strong> Medium global warming scenario<br>
+          <strong>4°C:</strong> High global warming scenario
+          </small>
+          """, unsafe_allow_html=True)
+
 
                   
 
