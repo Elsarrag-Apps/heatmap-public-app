e377272e3cf58282c3c75ae059cb38a4a2cf12a4
Elsarrag-Apps <esamelsarrag@hoarelea.com>
2025-05-07 20:15:12 +0100

    Update heatmap_app.py

--- heatmap_app.py
+++ heatmap_app.py
@@ -147,7 +147,7 @@ if mode == "Urban Heat Risk":
          
         if "utfvi" in st.session_state and show_utfvi:
             Map.addLayer(st.session_state.utfvi, {
-                'min': -0.005, 'max': 0.2,
+                'min': -0.005, 'max': 0.02,
                 'palette': ['blue', 'lightblue', 'lightgreen', 'yellow', 'orange', 'orangered', 'red'],
                 'opacity': utfvi_opacity
             }, 'UTFVI')
