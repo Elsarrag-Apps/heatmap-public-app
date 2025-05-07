2400c7d1cd016d6b62937be7187b99aca1cd0c7b
Elsarrag-Apps <esamelsarrag@hoarelea.com>
2025-05-07 11:19:03 +0100

    Update heatmap_app.py

--- heatmap_app.py
+++ heatmap_app.py
@@ -144,7 +144,7 @@ if mode == "Urban Heat Risk":
             Map.to_streamlit(width=700, height=500, scrolling=True, add_layer_control=True)
 
             st.markdown("#### 🔍 LST Color Legend")
-               st.markdown("""
+            st.markdown("""
                <div style="padding:10px">
                 <div style="display:flex;flex-direction:column;font-size:14px">
                   <div><span style="display:inline-block;width:15px;height:15px;background-color:darkblue;margin-right:6px;"></span> < 0°C</div>
